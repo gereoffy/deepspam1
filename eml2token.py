b500a6b8cfcfbd9dd54719e8591402a61d08169b
arpitest <arpi@thot.banki.hu>
2020-08-22 12:00:00 +0000

    v0.8: attachment file parsing:  docx, rtf (striprtf, included), pdf (requires pdftotext or pdfminer), ms-tnef (using tnefparse+compressed_rtf)

--- eml2token.py
+++ eml2token.py
@@ -5,11 +5,44 @@ import io
 #import os
 import sys
 
+import time
+
 #import errno
 #import mimetypes
 #from html2text import html2text
 import re
 import traceback
+
+# docx olvasashoz:
+import zipfile
+
+try:
+  import pdftotext
+  pdf_support="pdftotext"
+except:
+  try:
+    import pdfminer.high_level
+    pdf_support="pdfminer"
+  except:
+    pdf_support=None
+
+try:
+  from striprtf import rtf_to_text
+  rtf_support=1
+except:
+  rtf_support=0
+
+try:
+  from tnefparse import TNEF
+  tnef_support=1  # TNEF/HTML support
+  if rtf_support:
+    try:
+      import compressed_rtf
+      tnef_support=2  # TNEF/RTF support
+    except:
+      pass
+except:
+  tnef_support=0
 
 
 def eprint(*args, **kwargs):
@@ -19,6 +52,17 @@ def eprint(*args, **kwargs):
 import email
 import codecs
 
+from email.header import decode_header,make_header
+
+def hdrdecode(h):
+    try:
+        return unicode(make_header(decode_header(h.replace("?==?","?= =?"))))
+    except NameError:
+        pass
+    try:
+        return str(make_header(decode_header(h)))
+    except:
+        return h
 
 from htmlentitydefs import name2codepoint
 
@@ -118,17 +162,21 @@ def remove_url(text):
 
 confusables={}
 
+def load_unicodes(fnev):
+    # try to load unicodes.map from file
+    for line in io.open(fnev,"rt",encoding="utf-8",errors="ignore"):
+        l=line.rstrip("\n\r").split("\t",1)
+        confusables[ord(l[0])]=l[1]
+    eprint("%d entries loaded from %s file" %(len(confusables),fnev))
+
+import unicodedata
+
 def remove_accents(input_str):
     if len(confusables)==0:
         try:
-            # try to load unicodes.map from file
-            for line in io.open("unicodes.map","rt",encoding="utf-8",errors="ignore"):
-                l=line.rstrip("\n\r").split("\t",1)
-                confusables[ord(l[0])]=l[1]
-            eprint("%d entries loaded from unicodes.map file" %(len(confusables)))
+            load_unicodes("unicodes.map")
         except:
             # generate it with normalize() (without confusables...)
-            import unicodedata
             for ic in range(128,0x20000):
                 nfkd_form = unicodedata.normalize('NFKD', chr(ic))
                 oc=nfkd_form.encode('ASCII', 'ignore').decode('ASCII', 'ignore')
@@ -156,6 +204,7 @@ def eml2str(msg):
     eprint(type(msg))
 
   text = []
+  textlen=0
   #pp = msg.get_payload()
   for p in msg.walk():
 #    print p.get_content_type()
@@ -176,8 +225,9 @@ def eml2str(msg):
     elif charset[0:4]=="utf8":
       charset="utf-8"
     ctyp=p.get_content_type().lower()
+    fnev=hdrdecode(str(p.get_filename())).lower()
     disp=p.get_content_disposition()
-#    print((ctyp,disp,charset))
+#    print((ctyp,disp,fnev))
     if ctyp.split('/')[0]=="text" and disp!="attachment":
 #      print(ctyp)
 #      if ctyp.find("rfc")>=0:
@@ -200,6 +250,66 @@ def eml2str(msg):
           text.append(data)
         elif ctyp=="text/plain":
           text.append(data)
+        if textlen<len(data):
+          textlen=len(data)
+      except:
+        eprint(traceback.format_exc())
+    elif textlen<200:
+      s=""
+      t0=time.time()
+      try:
+        if (ctyp=="application/pdf" or fnev.endswith(".pdf")) and pdf_support:
+          eprint("PDF: parsing file: "+fnev)
+          if pdf_support=="pdfminer":
+            s=pdfminer.high_level.extract_text( io.BytesIO(p.get_payload(decode=True)) , maxpages=3 )
+          elif pdf_support=="pdftotext":
+            pdf = pdftotext.PDF(io.BytesIO(p.get_payload(decode=True)))
+            for page in pdf:
+              s=str(page)
+              if len(s)>200:
+                break
+        elif (ctyp=="application/rtf" or fnev.endswith(".rtf")) and rtf_support:
+          eprint("RTF: parsing file: "+fnev)
+          s = rtf_to_text(p.get_payload(decode=True).decode("utf-8","ignore"))
+        elif ctyp=="application/vnd.openxmlformats-officedocument.wordprocessingml.document" or fnev.endswith(".docx"):
+          eprint("DOCX: parsing file: "+fnev)
+          zipf=zipfile.ZipFile(io.BytesIO(p.get_payload(decode=True)))
+          html=zipf.read('word/document.xml').decode("utf-8")
+          for ret in html.split("<"):
+            try:
+                tag,txt=ret.split(">",1)
+                tag1=tag.split()[0]
+            except:
+                continue
+            if tag1=="w:t":
+                s+=txt
+            elif tag1 in ["w:tab","w:br","w:cr","w:p"]:
+                s+="\t"
+        elif (ctyp=="application/ms-tnef" or fnev=="winmail.dat") and tnef_support:
+#          print("TNEF: parsing file: "+fnev+" from "+msg.get("Message-id","N/A"))
+          eprint("TNEF: parsing file: "+fnev)
+          tnefobj = TNEF(p.get_payload(decode=True))
+          tnefcp=tnefobj.codepage if tnefobj.codepage else "cp1250"
+#          if tnefobj.body:
+#              print("TNEF.raw:  %d" %(len(tnefobj.body)))
+          if tnefobj.htmlbody:
+#              print("TNEF.html: %d" %(len(tnefobj.htmlbody)))
+#              print(type(tnefobj.htmlbody))
+#              if b"charset=utf-8" in tnefobj.htmlbody:
+              try:
+                  s=html2text(tnefobj.htmlbody.decode("utf-8","strict"))
+#                  print("UTF8 detected in TNEF/HTML...")
+              except:
+                  s=html2text(tnefobj.htmlbody.decode(tnefcp,"ignore"))
+          elif tnef_support>1 and tnefobj.rtfbody:
+#              print(type(tnefobj.rtfbody))
+#              print("TNEF.rtf:  %d" %(len(tnefobj.rtfbody)))
+              s = rtf_to_text(tnefobj.rtfbody.decode(tnefcp,"ignore"))
+        t0=time.time()-t0
+        print(s)
+        if len(s)>50:
+          eprint("parsed: %d chars, %d ms"%(len(s),t0*1000))
+          text.append(s)
       except:
         eprint(traceback.format_exc())
   return text
