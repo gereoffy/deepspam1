1716f4a1ddcccbe89dcbfcf0b3dcb295d1daadd6
gereoffy <arpi.git@interoot.hu>
2023-06-29 19:15:22 +0000

    updated from upstream: https://github.com/joshy/striprtf/blob/master/striprtf/striprtf.py

--- striprtf.py
+++ striprtf.py
@@ -1,7 +1,5 @@
-
-# https://github.com/joshy/striprtf/blob/master/striprtf/striprtf.py
-
 import re
+import codecs
 """
 Taken from https://gist.github.com/gilsondev/7c1d2d753ddb522e7bc22511cfb08676
 and modified for better output of tables.
@@ -17,7 +15,7 @@ destinations = frozenset((
     'colortbl','comment','company','creatim','datafield','datastore','defchp','defpap',
     'do','doccomm','docvar','dptxbxtext','ebcend','ebcstart','factoidname','falt',
     'fchars','ffdeftext','ffentrymcr','ffexitmcr','ffformat','ffhelptext','ffl',
-    'ffname','ffstattext','field','file','filetbl','fldinst','fldrslt','fldtype',
+    'ffname','ffstattext','file','filetbl','fldinst','fldtype',
     'fname','fontemb','fontfile','fonttbl','footer','footerf','footerl','footerr',
     'footnote','formfield','ftncn','ftnsep','ftnsepc','g','generator','gridtbl',
     'header','headerf','headerl','headerr','hl','hlfr','hlinkbase','hlloc','hlsrc',
@@ -70,27 +68,82 @@ specialchars = {
     "bullet": "\u2022",
     "lquote": "\u2018",
     "rquote": "\u2019",
-    "ldblquote": "\201C",
+    "ldblquote": "\u201C",
     "rdblquote": "\u201D",
     "row": "\n",
     "cell": "|",
     "nestcell": "|",
-    }
+    "~": "\xa0",
+    "\n":"\n",
+    "\r": "\r",
+    "{": "{",
+    "}": "}",
+    "\\": "\\",
+    "-": "\xad",
+    "_": "\u2011"
+
+}
 
 PATTERN = re.compile(
     r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)",
-    re.I,
+    re.IGNORECASE,
 )
 
+HYPERLINKS = re.compile(
+    r"(\{\\field\{\n?\\\*\\fldinst\{.*HYPERLINK\s(\".*\")\}{2}\s?\{.*\s+(.*)\}{2})",
+    re.IGNORECASE
+)
 
-def rtf_to_text(text):
+def _is_hyperlink(match):
+        groups = match.groups()
+        link_text = groups[-1]
+        link_destination = groups[-2]
+        # why this ugly hack? the hyperlink regex can't cope with the regex ending with
+        # either }}} or }}. So we are capturing two and if there is another
+        # one we remove it here
+        if link_text[-1] == "}":
+            g1 = link_text[:-1]
+        else:
+            g1 = link_text
+        return f"{g1}({link_destination})"
+
+
+def _replace_hyperlinks(text):
+    return re.sub(HYPERLINKS, _is_hyperlink, text)
+
+    
+def rtf_to_text(text, encoding="cp1252", errors="strict"):
+    """ Converts the rtf text to plain text.
+
+    Parameters
+    ----------
+    text : str
+        The rtf text
+    encoding : str
+        Input encoding which is ignored if the rtf file contains an explicit codepage directive, 
+        as it is typically the case. Defaults to `cp1252` encoding as it the most commonly used.
+    errors : str
+        How to handle encoding errors. Default is "strict", which throws an error. Another
+        option is "ignore" which, as the name says, ignores encoding errors.
+
+    Returns
+    -------
+    str
+        the converted rtf text as a python unicode string
+    """
+    text = _replace_hyperlinks(text)
     stack = []
     ignorable = False  # Whether this group (and all inside it) are "ignorable".
     ucskip = 1  # Number of ASCII characters to skip after a unicode character.
     curskip = 0  # Number of ASCII characters left to skip
-    out = []  # Output buffer.
+    hexes = None
+    out = ''
+
     for match in PATTERN.finditer(text):
-        word, arg, hex, char, brace, tchar = match.groups()
+        word, arg, _hex, char, brace, tchar = match.groups()
+        if hexes and not _hex:
+            out += bytes.fromhex(hexes).decode(encoding=encoding, errors=errors)
+            hexes = None
         if brace:
             curskip = 0
             if brace == "{":
@@ -98,38 +151,36 @@ def rtf_to_text(text):
                 stack.append((ucskip, ignorable))
             elif brace == "}":
                 # Pop state
-                try:
+                if stack:
                     ucskip, ignorable = stack.pop()
                 # sample_3.rtf throws an IndexError because of stack being empty.
                 # don't know right now how this could happen, so for now this is
                 # a ugly hack to prevent it
-                except IndexError:
+                else:
                     ucskip = 0
                     ignorable = True
         elif char:  # \x (not a letter)
             curskip = 0
-            if char == "~":
+            if char in specialchars:
                 if not ignorable:
-                    out.append("\xA0")  # NBSP
-            elif char in "{}\\":
-                if not ignorable:
-                    out.append(char)
+                   out += specialchars[char]
             elif char == "*":
                 ignorable = True
-            elif char == "\n":
-                if not ignorable:
-                    out.append("\x0A")  # LF
-            elif char == "\r":
-                if not ignorable:
-                    out.append("\x0D")  # CR
         elif word:  # \foo
             curskip = 0
             if word in destinations:
                 ignorable = True
-            elif ignorable:
+            # http://www.biblioscape.com/rtf15_spec.htm#Heading8
+            elif word == "ansicpg":
+                encoding = f"cp{arg}"
+                try:
+                    codecs.lookup(encoding)
+                except LookupError:
+                    encoding = "utf8"
+            if ignorable:
                 pass
             elif word in specialchars:
-                out.append(specialchars[word])
+                out += specialchars[word]
             elif word == "uc":
                 ucskip = int(arg)
             elif word == "u":
@@ -140,23 +191,20 @@ def rtf_to_text(text):
                     c = int(arg)
                     if c < 0:
                         c += 0x10000
-                    if c > 127:
-                        out.append(chr(c))  # NOQA
-                    else:
-                        out.append(chr(c))
+                    out += chr(c)
                     curskip = ucskip
-        elif hex:  # \'xx
+        elif _hex:  # \'xx
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
-                c = int(hex, 16)
-                if c > 127:
-                    out.append(chr(c))  # NOQA
+                c = int(_hex, 16)
+                if not hexes:
+                    hexes = _hex
                 else:
-                    out.append(chr(c))
+                    hexes += _hex
         elif tchar:
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
-                out.append(tchar)
-    return "".join(out)
+                out += tchar
+    return out
